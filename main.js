ef1b975f9ebf3b365d7296770df031ee2637b4bb
zjs6631 <zacharyjonessmith@gmail.com>
2022-07-23 15:44:51 -0400

    added logic to allow for status toggle

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 const newBook = document.getElementById("newBook");
 const libraryBody = document.getElementById("libraryBody");
 const bookModal = document.getElementById("bookModal");
@@ -9,11 +11,32 @@ const pagesInput = document.getElementById("pages");
 const completedInput = document.getElementById("completed");
 
 
+
+
+
 function Book(title, author, pages, completed) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.completed = completed
+    this.completed = completed;
+}
+
+Book.prototype.readStatus = function() {
+    if(this.completed == true){
+        return "Completed";
+    } else {
+        return "Incomplete"
+    }
+}
+
+Book.prototype.changeStatus = function(){
+    if(this.completed == true){
+        
+        return "Incomplete";
+    } else {
+        
+        return "Completed";
+    }
 }
 
 function displayBook() {
@@ -24,17 +47,42 @@ function displayBook() {
     for(let i = 0; i < library.length; i++){
         const createDiv = document.createElement("div");
         createDiv.classList.add("book");
-        createDiv.innerHTML =  "Title: " + library[i].title + "<br> Author: " + library[i].author + "<br> Pages:" + library[i].pages + "<br> Completed" + library[i].completed;
+        createDiv.innerHTML =  "Title: " + library[i].title + "<br> Author: " + library[i].author + "<br> Pages:" + library[i].pages +
+         "<br> Completion Status: " + library[i].readStatus();
+        const statusBtn = document.createElement("button")
+        statusBtn.classList.add("statusBtn");
+        statusBtn.innerHTML = "Toggle Status"
+        statusBtn.addEventListener("click",()=>{
+            createDiv.innerHTML =  "Title: " + library[i].title + "<br> Author: " + library[i].author + "<br> Pages:" + library[i].pages +
+            "<br> Completion Status: " + library[i].changeStatus();
+            if(library[i].completed == true){
+                library[i].completed = false;
+            } else {
+                library[i].completed = true;
+            }
+            const statusBtn = document.createElement("button")
+            statusBtn.classList.add("statusBtn");
+            statusBtn.innerHTML = "Toggle Status"
+            createDiv.appendChild(statusBtn);
+        })
         document.getElementById("librarybody").appendChild(createDiv);
+        createDiv.appendChild(statusBtn);
+
         
     }
 }
 
-let demoBook = new Book("The wild thing", "William B Queasy", "203", "No");
+
+
+let demoBook = new Book("The wild thing", "William B Queasy", "203", true);
 
 let library = [demoBook];
 
 displayBook();
+
+
+
+
 
 submitBtn.addEventListener("click", ()=>{
     let newBook = new Book(titleInput.value, authorInput.value,
@@ -48,6 +96,9 @@ cancelBtn.addEventListener("click",()=>{
     bookModal.style.display = "none";
     console.log("we did it!")
 })
+
+
+
 newBook.addEventListener("click", getBook);
 
 function getBook(){
