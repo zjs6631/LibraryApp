6225c33a5e099d67c347511617fce882b4454e51
zjs6631 <zacharyjonessmith@gmail.com>
2022-07-29 15:51:43 -0400

    refactored code to use classes for book object, fixed status logic

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # LibraryApp
 Library app to hold book objects and their properties
+This Library app is built using HTML/CSS/Javascript
+
+-- I refactored the JS to use classes to create the book objects
+-- Each book has attributes for name, author, pages, and completion status
+-- This information is collected via a modal form and displayed on a grid tile for the user
+-- status button allows user to mark book as completed/in progress
+
+--a few things to refactor in the future would be to just create the book object and see if it is possible to just append it to the grid
+  --and if it will fall into place on the grid
+--another thing to refactor would be to tweak the grid sizing to make it more dynamic. It is currently a 4x4 grid. Decided to keep things simple while working out the Javascript logic.
+--one more thing will be to re-do the UI/UX to make it more appealing. image background came out a little tacky, so will address this in the future. 
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+//grabbed several DOM elements that will be used in the JS
 const newBook = document.getElementById("newBook");
 const libraryBody = document.getElementById("libraryBody");
 const bookModal = document.getElementById("bookModal");
@@ -9,49 +9,52 @@ const titleInput = document.getElementById("title");
 const authorInput = document.getElementById("author");
 const pagesInput = document.getElementById("pages");
 const completedInput = document.getElementById("completed");
+const radioCompleted = document.getElementById("completed");
+const radioInProgress = document.getElementById("InProgress");
 
-
-
-
-
-function Book(title, author, pages, completed) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.completed = completed;
-}
-
-Book.prototype.readStatus = function() {
-    if(this.completed == true){
-        return "Completed";
-    } else {
-        return "Incomplete"
+//refactored the code to utilize classes for book creation
+class Book {
+    constructor(title, author, pages, completed){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.completed = completed;
+    }
+    //moved several functions into the class
+    //read status just reads the current status the book is set to
+    readStatus(){
+        if(this.completed === true){
+            return "Completed";
+        } else {
+            return "In Progress";
+        }
+    }
+    //change status looks at current status and changes the value accordingly
+    changeStatus(){
+        if(this.completed === true){
+            return "Incompleted";
+        } else {
+            return "In Progress";
+        }
     }
 }
 
-Book.prototype.changeStatus = function(){
-    if(this.completed == true){
-        
-        return "Incomplete";
-    } else {
-        
-        return "Completed";
-    }
-}
-
+//display book actually puts the book object on the page
 function displayBook() {
-    let books = document.querySelectorAll(".book");
-    books.forEach(book =>{
+    let books = document.querySelectorAll(".book"); //select all books
+    books.forEach(book =>{ //remove all of the books
         book.remove();
     })
-    for(let i = 0; i < library.length; i++){
-        const createDiv = document.createElement("div");
+    for(let i = 0; i < library.length; i++){ //loop through the book array and add them back to the page
+        const createDiv = document.createElement("div"); //create a div with class of "book"
         createDiv.classList.add("book");
+        //create the innerHTML for the book item
         createDiv.innerHTML =  "<p>Title: " + library[i].title + "</p><p>Author: " + library[i].author + "</p><p>Pages:" + library[i].pages +
          "</p><p id='status" + i + "'> Completion Status: " + library[i].readStatus() + "</p>";
-        const statusBtn = document.createElement("button")
+        const statusBtn = document.createElement("button") //add a status button and give it its class
         statusBtn.classList.add("statusBtn");
         statusBtn.innerHTML = "Toggle Status"
+        //add a listener to the button that will change the objects value in the array and on the screen display
         statusBtn.addEventListener("click",()=>{
             const statusText = document.getElementById("status" + i);
             if(library[i].completed == true){
@@ -67,32 +70,43 @@ function displayBook() {
                 library[i].completed = false;
             }
         })
+        //append the div as a child to the library body
         document.getElementById("librarybody").appendChild(createDiv);
-        createDiv.appendChild(statusBtn);
+        createDiv.appendChild(statusBtn); //append the status button to that div also
         
     }
 }
 
 
 
-let demoBook = new Book("The wild thing", "William B Queasy", "203", true);
+let demoBook = new Book("The wild thing", "William B Queasy", "203", true); //demo book to format display easier
 
-let library = [demoBook];
+let library = [demoBook]; //library array containing demobook obj
 
-displayBook();
-
-
+displayBook(); //displayBook used to check current display functionality 
 
 
 
+
+
+//add submit button listener to collect a new books attributes and create an object from them
 submitBtn.addEventListener("click", ()=>{
+    let res = true;
+    if(radioCompleted.checked === true && radioInProgress.checked === true){
+        res = false;
+    } else if(radioCompleted.checked === true){
+        res = true;
+    } else {
+        res = false;
+    }
     let newBook = new Book(titleInput.value, authorInput.value,
-    pagesInput.value, completedInput.value)
+    pagesInput.value, res)
     library.push(newBook);
-    bookModal.style.display = "none";
-    displayBook()
+    bookModal.style.display = "none"; //hide the modal form again
+    console.log(radioCompleted.checked +" "+ radioInProgress.checked + " " + res)
+    displayBook() //call display book to add the book to the page
 })
-
+ //cancel button hides the modal
 cancelBtn.addEventListener("click",()=>{
     bookModal.style.display = "none";
     console.log("we did it!")
@@ -100,16 +114,17 @@ cancelBtn.addEventListener("click",()=>{
 
 
 
-newBook.addEventListener("click", getBook);
 
+//add button calls the getBook function
+newBook.addEventListener("click", getBook);
+ //getBook() resets all field values 
 function getBook(){
 
     bookModal.style.display = "flex";
     titleInput.value = "";
     authorInput.value = "";
     pagesInput.value = "";
-    completedInput.value;
-
-    
+    radioCompleted.checked = false;
+    radioInProgress.checked = false;   
     
 }
